b94d2976d53541958dd3983ca5a15d8d0b7be746
Ryan Wold <wold@afomi.com>
2024-03-07 16:32:46 -0800

    YoursIcon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./context/PandaWalletContext";
 export * from "./hook/usePandaWallet";
 export * from "./types/providerTypes";
-export * from "./components/PandaIcon";
+export * from "./components/YoursIcon";
